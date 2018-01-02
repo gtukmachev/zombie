417941e8204c8a6573b0bb091e7019063529b397
Grigory <gtukmachev@gmail.com>
2018-01-02 19:34:09 +0700

    game core refactoring: comments

--- src/lib/game-core/model/movers/abstract-phisic-mover.ts
+++ src/lib/game-core/model/movers/abstract-phisic-mover.ts
@@ -4,16 +4,12 @@ import {Vector} from '../../vector';
 export abstract class AbstractPhisicMover extends Mover {
 
   public move(): void {
-
-    const target = this.targetPoint() || this.gObj.p;
-
-    if (this.gObj.p.x === target.x && this.gObj.p.y === target.y) { //it's mean: we have to brake and stop
-       if (this.gObj.sVal === 0) {
-         return; // the object already stopped - return
-       }
-    }
-
-
+      const target = this.targetPoint() || this.gObj.p;
+      if (this.gObj.p.x === target.x && this.gObj.p.y === target.y) { //it's mean: we have to brake and stop
+          if (this.gObj.sVal === 0) {
+           return; // the object already on place - finish movement // todo: make 'brake' if |speed| more than 0
+          }
+      }
   }
 
   abstract targetPoint(): Vector;
