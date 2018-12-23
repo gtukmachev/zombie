a1b59553c396daf7009b742dad136673c2373ef2
Grigory <grigory@clearscale.net>
2018-12-23 12:45:08 +0700

    Matrix index optimization

--- src/lib/game-core/matrix/location-matrix.ts
+++ src/lib/game-core/matrix/location-matrix.ts
@@ -2,130 +2,139 @@ import {Vector} from '../vector';
 import {GameObj} from '../objects/game-obj';
 
 export class LocationMatrix {
-
+  
   public size;
-
-  public index: Array<Array<Array<GameObj>>>; // map matrix cel (l,c) -> set of objects inside this ceil
-  public positions: MatrixPosIndex= {}; // map object -> ceils of the matrix where this project present
-
+  
+  public index: Array<Array<HashSetOfGameObjects>>; // map matrix cel (l,c) -> set of objects inside this ceil
+  public positions: MatrixPosIndex = {}; // map object -> ceils of the matrix where this project present
+  
   public lines: number;
   public columns: number;
-
+  
   private worldSize: Vector;
-
+  
   constructor(size, worldSize: Vector) {
     this.size = size;
     this.worldSize = worldSize.copy();
     this.forceClear();
   }
-
+  
   public forceClear(): void {
     this.lines = Math.ceil(this.worldSize.y / this.size) + 1;
     this.columns = Math.ceil(this.worldSize.x / this.size) + 1;
-
+    
     this.index = [];
     for (let l = 0; l < this.lines; l++) {
       this.index[l] = [];
-      for (let c=0; c < this.columns; c++) {
-        this.index[l][c] = [];
+      for (let c = 0; c < this.columns; c++) {
+        this.index[l][c] = {};
       }
     }
-
+    
     this.positions = {};
   }
-
+  
   public remove(o: GameObj): void {
     const id = `${o.id}`;
     const prevPositions = this.positions[id];
     if (prevPositions) {
-      for (let l = prevPositions.lb; l <= prevPositions.le; l++){
+      for (let l = prevPositions.lb; l <= prevPositions.le; l++) {
         let prevIndexLine = this.index[l];
         if (prevIndexLine) {
           for (let c = prevPositions.cb; c <= prevPositions.ce; c++) {
-
+            
             const prevIndex = prevIndexLine[c];
             if (prevIndex) {
               this.removeFromSet(prevIndex, o);
             }
-
+            
           }
         }
       }
     }
-
+    
     this.positions[id] = undefined;
   }
-
+  
   public update(o: GameObj): void {
     if (!(o.outerFrame)) return;
-
+    
     const id = `${o.id}`;
-
-    const pTopY  = o.p.y + o.outerFrame.y;
+    
+    const pTopY = o.p.y + o.outerFrame.y;
     const pLeftX = o.p.x + o.outerFrame.x;
-
+    
     const prevPositions: MatrixPos = this.positions[id];
     const newPositions: MatrixPos = {
-      lb: Math.floor( pTopY                   / this.size), cb: Math.floor( pLeftX                   / this.size),
+      lb: Math.floor(pTopY / this.size), cb: Math.floor(pLeftX / this.size),
       le: Math.floor((pTopY + o.outerFrame.h) / this.size), ce: Math.floor((pLeftX + o.outerFrame.w) / this.size)
     };
-
+    
     if (prevPositions && newPositions.lb === prevPositions.lb && newPositions.cb === prevPositions.cb
-                      && newPositions.le === prevPositions.le && newPositions.ce === prevPositions.ce
+      && newPositions.le === prevPositions.le && newPositions.ce === prevPositions.ce
     ) {
       return;
     }
-
-    this.applyForRegion(prevPositions, cellObjectsSet => this.removeFromSet(cellObjectsSet, o));
-    this.applyForRegion(newPositions , cellObjectsSet => this.addIntoSet(cellObjectsSet, o));
-
+    
+    // todo: replace "REMOVE-at-all and ADD again" algorithm to INCREMENTAL matrix updating
+    this.applyForSetsOfRegion(prevPositions, cellObjectsSet => this.removeFromSet(cellObjectsSet, o));
+    this.applyForSetsOfRegion(newPositions, cellObjectsSet => this.addIntoSet(cellObjectsSet, o));
+    
     this.positions[id] = newPositions;
   }
-
-  public applyForNearestObjects(o: GameObj, callback: (GameObj) => void): void {
-    this.applyForRegionObjects(this.positions[`${o.id}`], callback);
+  
+  public applyForNearestObjects(o: GameObj, callbackForGameObj: (GameObj) => void): void {
+    this.applyForRegionObjects(this.positions[`${o.id}`], callbackForGameObj);
   }
-
-  public applyForRegion(pos: MatrixPos, callback: (Array) => void) {
-      if (pos) {
-          for (let l = pos.lb; l <= pos.le; l++) {
-              let line = this.index[l];
-              if (line) {
-                  for (let c = pos.cb; c <= pos.ce; c++) {
-                      const col = line[c];
-                      if (col) {
-                        callback(col);
-                      }
-                  }
-              }
+  
+  public applyForSetsOfRegion(pos: MatrixPos, callbackForGameObjSet: (HashSetOfGameObjects) => void) {
+    if (pos) {
+      for (let l = pos.lb; l <= pos.le; l++) {
+        let line = this.index[l];
+        if (line) {
+          for (let c = pos.cb; c <= pos.ce; c++) {
+            const objectsInTheMatrixCell: HashSetOfGameObjects = line[c];
+            if (objectsInTheMatrixCell) {
+              callbackForGameObjSet(objectsInTheMatrixCell);
+            }
           }
+        }
       }
+    }
   }
-
-  public applyForRegionObjects(pos: MatrixPos, callback: (GameObj) => void) {
-    this.applyForRegion(pos, index => index.forEach(callback));
+  
+  public applyForRegionObjects(pos: MatrixPos, callbackForGameObj: (GameObj) => void) {
+    this.applyForSetsOfRegion(pos, (gameObjectsInCell: HashSetOfGameObjects) => {
+      for (let objId in gameObjectsInCell) {
+        callbackForGameObj(gameObjectsInCell[objId]);
+      }
+    });
   }
-
-
+  
+  
   // todo: try to replace the Array<GameObj> to MapObject of <GameObj.id>: interface
-  private addIntoSet(set: Array<GameObj>, o: GameObj): void {
-    let i = 0;
-    while (i < set.length && set[i] !== o) { i++; }
-    if (i == set.length) set.push(o);
+  private addIntoSet(set: HashSetOfGameObjects, o: GameObj): void {
+    set[`${o.id}`] = o;
   }
-
-  // todo: try to replace the Array<GameObj> to MapObject of <GameObj.id>
-  private removeFromSet(set: Array<GameObj>, o: GameObj): void {
-    let i = 0;
-    while (i < set.length && set[i] !== o) { i++; }
-    if (i < set.length)  set.splice(i);
+  
+  private removeFromSet(set: HashSetOfGameObjects, o: GameObj): void {
+    delete set[`${o.id}`];
   }
-
-
-
+  
+  
 }
 
-interface MatrixPos {lb: number, cb: number, le: number, ce: number}
+interface MatrixPos {
+  lb: number,
+  cb: number,
+  le: number,
+  ce: number
+}
+
 interface MatrixPosIndex {
   [key: string]: MatrixPos
+}
+
+export interface HashSetOfGameObjects {
+  [key: string]: GameObj
 }
--- src/lib/game-core/matrix/matrix-visualizer-game-object.ts
+++ src/lib/game-core/matrix/matrix-visualizer-game-object.ts
@@ -1,53 +1,65 @@
 import {SimpleGameObj} from '../objects/simple-draw-game-obj';
+import {HashSetOfGameObjects} from './location-matrix';
 
 export class MatrixVisualizerGameObject extends SimpleGameObj {
-
+  
   public colorPassive: string | CanvasGradient | CanvasPattern;
   public colorActive: string | CanvasGradient | CanvasPattern;
   public font: string;
-
-  constructor (font:string, colorPassive: string | CanvasGradient | CanvasPattern, colorActive: string | CanvasGradient | CanvasPattern) {
-    super(0,0);
+  
+  constructor(font: string, colorPassive: string | CanvasGradient | CanvasPattern, colorActive: string | CanvasGradient | CanvasPattern) {
+    super(0, 0);
     this.colorPassive = colorPassive;
     this.colorActive = colorActive;
     this.font = font;
   }
-
-
+  
+  
   public draw(ctx: CanvasRenderingContext2D): void {
     ctx.lineWidth = 1;
     ctx.strokeStyle = this.colorPassive;
     ctx.textAlign = 'left';
     ctx.textBaseline = 'top';
     ctx.font = this.font;
-
+    
     const mx = this.game.worldSize.x;
     const my = this.game.worldSize.y;
     const s = this.game.matrix.size;
-
+    
     ctx.beginPath();
-    for( let x = 0; x < mx; x += s ) { ctx.moveTo(x, 0); ctx.lineTo(x, my); }
-    for( let y = 0; y < my; y += s ) { ctx.moveTo(0, y); ctx.lineTo(mx, y); }
+    for (let x = 0; x < mx; x += s) {
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, my);
+    }
+    for (let y = 0; y < my; y += s) {
+      ctx.moveTo(0, y);
+      ctx.lineTo(mx, y);
+    }
     ctx.stroke();
-
-
+    
+    
     ctx.strokeStyle = this.colorActive;
     ctx.fillStyle = this.colorActive;
     const index = this.game.matrix.index;
-
+    
     for (let l = 0; l < index.length; l++) {
       let line = index[l];
       for (let c = 0; c < line.length; c++) {
-        let objectsList = line[c];
-        if (objectsList.length > 0) {
-          ctx.beginPath();
-          ctx.rect(c*s, l*s, s, s);
-          ctx.stroke();
-
-          this.game.ctx.fillText(`${objectsList.length}`,c*s, l*s );
+        let objectsSet: HashSetOfGameObjects = line[c];
+        if (objectsSet) {
+          let count = 0;
+          for (let objId in objectsSet) {
+            count++;
+          }
+          if (count > 0) {
+            ctx.beginPath();
+            ctx.rect(c * s, l * s, s, s);
+            ctx.stroke();
+          }
+          this.game.ctx.fillText(`${count}`, c * s, l * s);
         }
       }
     }
-
+    
   }
 }
